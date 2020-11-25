ca4285b214e63fcb86e652ec0f8f1b40f707fe62
Anna Mnatsakanyan <mnatsakanyan901@gmail.com>
2020-11-25 12:00:09 +0400

    modified problem about arithmetic progression

--- homework_1/arithmetic_progression.py
+++ homework_1/arithmetic_progression.py
@@ -3,9 +3,5 @@ second_num = int(input("enter a second number of progression: "))
 N = int(input("enter an index of number: "))
 
 diff = second_num - first_num
-progression_list: list = [first_num, second_num]
-for i in range(N):
-    latest_num = progression_list[len(progression_list) - 1] + diff
-    progression_list.append(latest_num)
-
-print("The N-est number of progression is: " + str(progression_list[N - 1]))
+N_est_num = first_num + (N - 1) * diff
+print("The N-est number of progression is: " + str(N_est_num))
