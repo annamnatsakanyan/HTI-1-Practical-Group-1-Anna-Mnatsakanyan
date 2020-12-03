5ebfc68a5f400d63a6d4341d82f5feed2e561796
Anna Mnatsakanyan <mnatsakanyan901@gmail.com>
2020-12-03 19:37:42 +0400

    added Homework 2 task 1 is uppercase

--- Homework_2/is_uppercase.py
+++ Homework_2/is_uppercase.py
@@ -1,10 +1,10 @@
 word = input("enter the word: ").split()
 
 
-def is_uppercase(word):
+def is_uppercase(uppercase_word):
     word_uppercase = True
-    for i in range(len(word)):
-        if word[i] != word[i].upper():
+    for i in range(len(uppercase_word)):
+        if uppercase_word[i] != uppercase_word[i].upper():
             word_uppercase = False
             break
     return word_uppercase
