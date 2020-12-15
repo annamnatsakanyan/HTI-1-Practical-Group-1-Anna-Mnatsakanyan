609f5bf1ee9f4aaf47f6f7a635b37230b5d73dc4
Anna Mnatsakanyan <mnatsakanyan901@gmail.com>
2020-12-15 11:58:02 +0400

    Homework 2 task stools changes

--- Homework_2/stools.py
+++ Homework_2/stools.py
@@ -1,17 +1,11 @@
-students_height = input("enter the numbers: ").split()
-
-
 def stools(height):
-    for i in range(len(height)):
-        height[i] = int(height[i])
     sum = 0
-    max_elem = height[0]
-    for i in range(len(height)):
-        if height[i] > max_elem:
-            max_elem = height[i]
+    max_elem = max(students_height)
     for i in range(len(height)):
         sum += max_elem - height[i]
     return sum
 
+
+students_height = [int(elem) for elem in input("enter the heights: ").split()]
 
 print(stools(students_height))
