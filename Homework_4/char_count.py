b08a7a3d78adcce2c87ad13ee24454e9a4e0bd89
Anna Mnatsakanyan <mnatsakanyan901@gmail.com>
2020-12-16 11:07:27 +0400

    added Homework 4 task 1 char count

---
+++ Homework_4/char_count.py
@@ -0,0 +1,14 @@
+def char_count(text):
+    char_dict = {}
+    for elements in text:
+        if elements in text:
+            keys = char_dict.keys()
+            if elements in keys:
+                char_dict[elements] += 1
+            else:
+                char_dict[elements] = 1
+    return char_dict
+
+
+text_input = input("enter a text: ")
+print(char_count(text_input))
