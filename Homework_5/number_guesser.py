e73d0a0c6aedcd91746cc1929262fe9881930643
Anna Mnatsakanyan <mnatsakanyan901@gmail.com>
2020-12-25 12:55:26 +0400

    homework 5 task 1 changes

--- Homework_5/number_guesser.py
+++ Homework_5/number_guesser.py
@@ -1,20 +1,15 @@
-count = 0
-
-
-def number_guesser(start, end):
+def number_guesser(start, end, count=0):
     mid_num = (start + end) // 2
     print("is the number ", mid_num, "?")
 
     answer = int(input("enter answer: "))
-    global count
     while count <= 10:
-        count += 1
         if answer == 0:
             return "you find number"
         elif answer == -1:
-            return number_guesser(start, mid_num - 1)
+            return number_guesser(start, mid_num - 1, count+1)
         else:
-            return number_guesser(mid_num + 1, end)
+            return number_guesser(mid_num + 1, end, count+1)
     return 'you cheated in one of the steps'
 
 
